6bdae6debded32aff2d9313df2731665dfbedb51
Hamid Bekamiri <hamid.bekam@gmail.com>
2023-04-09 17:37:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from sklearn.neighbors import KNeighborsClassifier
 iris = load_iris()
 X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.2)
 
-model = KNeighborsClassifier(n_neighbors=3)
+model = KNeighborsClassifier(n_neighbors=5)
 model.fit(X_train, y_train)
 
 import streamlit as st
